d171c3e8b5c6a050a32fbdc460c480381b74adac
sebastian <shr110317@gmail.com>
2022-01-18 16:00:15 -0500

    Options and API ready

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './css/styles.css'
+import './css/animation.css'
+
+createApp(App).mount('#app')
+
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>¿Quiene es este Pokemon?</h1>
+        
+        <pokemon-picture :pokemonId="6" :showPokemon="true" />
+        <pokemon-options :pokemons="pokemonArr" />
+    </div>
+</template>
+
+<script>
+import PokemonOptions from '@/components/PokemonOptions.vue'
+import PokemonPicture from '@/components/PokemonPicture.vue'
+
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+// console.log(getPokemonOptions());
+
+
+export default {
+    name: 'PokemonPage',
+    components: {
+        PokemonOptions,
+        PokemonPicture
+    },
+    data(){
+        return{
+            pokemonArr: []
+        }
+    },
+    methods: {
+        async mixPokemonArray(){
+            this.pokemonArr = await getPokemonOptions();
+        }
+    },
+
+    mounted(){
+        this.mixPokemonArray()
+    }
+}
+</script>
